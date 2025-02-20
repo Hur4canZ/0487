d08d6489570a7179be98bc8c2820f859d9c205b6
Josep <dam1@A80533400715258>
2025-02-20 13:59:51 +0100

    Tercera versió del projecte

--- CalduchJosep_0487/src/Main/eclipse.java
+++ CalduchJosep_0487/src/Main/eclipse.java
@@ -5,6 +5,7 @@ public class eclipse {
 	public static void main(String[] args) {
 		System.out.println("1a modificació Git");
 		System.out.println("2a modificació Git");
+		System.out.println("3a modificacióGit");
 	}
 
 }

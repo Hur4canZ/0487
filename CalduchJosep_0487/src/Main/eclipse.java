9e59fdeb5caca88b405e9a51b72ce886ff129aca
Hur4canZ <jtcalvid15@insgabrielamistral.cat>
2025-02-20 14:16:43 +0100

    4a versió eclipse.java

--- CalduchJosep_0487/src/Main/eclipse.java
+++ CalduchJosep_0487/src/Main/eclipse.java
@@ -5,7 +5,8 @@ public class eclipse {
 	public static void main(String[] args) {
 		System.out.println("1a modificació Git");
 		System.out.println("2a modificació Git");
-		System.out.println("3a modificacióGit");
+		System.out.println("3a modificació Git");
+		System.out.println("4a modificació Git Branca2");
 	}
 
 }
